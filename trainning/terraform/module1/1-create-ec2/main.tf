eec94bba34d20b60316a278752683b24b4b8147b
Mario Costa <mario.costa@trustly.com>
2022-07-21 12:54:25 -0300

    First commit
    remove some terraform files
    
    remove some terraform files
    
    rename .gitignore file
    
    Add .gitignore
    
    gitignore fix

---
+++ trainning/terraform/module1/1-create-ec2/main.tf
@@ -0,0 +1,57 @@
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_instance" "test" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloTerraform"
+  }
+
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh_terraform"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [data.aws_vpc.default.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_terraform"
+  }
+}
+
---
+++ trainning/terraform/module1/3-create-ec2-with-remote-backend/main.tf
@@ -0,0 +1,56 @@
+# About "data"
+# Reference: https://learn.hashicorp.com/tutorials/terraform/data-sources
+##############################################################################
+# Terraform uses data sources to fetch information from cloud provider APIs,
+# such as disk image IDs, or information about the rest of your infrastructure
+# through the outputs of other Terraform configurations.
+# Data sources allow you to load data from APIs or other Terraform workspaces.
+##############################################################################
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/ami
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = [var.aws_ami_owners]
+
+  dynamic "filter" {
+    for_each = var.aws_instance_filter_tags
+    iterator = tag
+    content {
+      name   = tag.key
+      values = [tag.value]
+    }
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc
+data "aws_vpc" "default" {
+  default = true
+}
+
+# About "resource"
+# Reference: https://www.terraform.io/language/resources
+###################################################################################
+# Resources are the most important element in the Terraform language.
+# Each resource block describes one or more infrastructure objects,
+# such as virtual networks, compute instances, or higher-level components
+# such as DNS records. Resource Blocks documents the syntax for declaring resources.
+###################################################################################
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+resource "aws_instance" "test" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.aws_instance_type
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  tags                   = var.aws_instance_tags
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh_terraform"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+  ingress     = var.aws_sg_ingress_rule
+  egress      = var.aws_sg_egress_rule
+  tags        = var.aws_sg_tags
+}
+
