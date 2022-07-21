aef7ce7820b8a94b8d35eec28e3af882e4b96a6c
Mario Costa <mario.costa@trustly.com>
2022-07-21 13:59:10 -0300

    Fix typo

--- trainning/terraform/module1/3-create-ec2-with-remote-backend/terraform.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "hello-terraform-state-backend"
-    key            = "terraform.tfstate"
-    region         = "us-west-2"
-    dynamodb_table = "hello_terraform_state"
-  }
-}
