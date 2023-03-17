30e665433ef83b4f65fa5f8306a3381f08e362ae
Mario Costa <mario.costa@trustly.com>
2023-03-17 08:51:22 -0300

    remove some comments

--- training/terraform/class-2/locals.tf
+++ training/terraform/class-2/locals.tf
@@ -11,7 +11,6 @@ locals {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   ##### network #####
-  # Hard defined variable data-type
   aws_sg_ingress_rule = [{
     description      = "SSH from VPC"
     from_port        = 22
@@ -23,7 +22,7 @@ locals {
     security_groups  = []
     self             = false
   }]
-  # More flexibel way to declare a variable
+
   aws_sg_egress_rule = [{
     description      = "All traffic"
     from_port        = 0
@@ -38,18 +37,20 @@ locals {
   aws_sg_tags = {
     Name = "allow_ssh_terraform"
   }
-  # String data type
+
   aws_instance_type = "t3.micro"
-  # Boolean data type
+
   aws_instance_create = true
-  # Any indicates that we can have any kind of data inside our map()
+
   aws_instance_filter_tags = {
     "name"                = "ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"
     "virtualization-type" = "hvm"
   }
+
   aws_instance_tags = {
     Name = "HelloTerraform"
   }
+
   # Canonical Ubuntu account id
   aws_ami_owners = "099720109477"
 }
